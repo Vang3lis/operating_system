beb7bc3265660a1e29ba3cfdcbe6ab4d31e4b776
Vang3lis <852763852@qq.com>
2020-07-22 14:52:44 +0800

    gdt structure

--- 09-32bit-gdt/32bit-gdt.asm
+++ 09-32bit-gdt/32bit-gdt.asm
@@ -10,7 +10,7 @@ gdt_code: ; the code segment descriptor
 ; 1st flags: (present)1 (privilege)00 (descriptor type)1 -> 1001b
 ; type flags: (code)1 (conforming)0 (readable)1 (accessed)0 -> 1010b
 ; 2nd flags: (granularity)1 (32-bit default)1 (64-bit seg)0 (AVL)0 -> 1100b
-	dw 0xfffff		; Limit (bits 0-15)
+	dw 0xffff		; Limit (bits 0-15)
 	dw 0x0			; Base (bits 0-15)
 	db 0x0			; Base (bits 16-23)
 	db 10011010b	; 1st flags, type flags
@@ -20,7 +20,7 @@ gdt_code: ; the code segment descriptor
 gdt_data: ; the data segment descriptor
 ; same as code segment except for the type flags
 ; type flags: (code)0 (expand down)0 (writeable)1 (accessed)0 -> 0010b
-	dw 0xfffff		; Limit (bits 0-15)
+	dw 0xffff		; Limit (bits 0-15)
 	dw 0x0			; Base (bits 0-15)
 	db 0x0			; Base (bits 16-23)
 	db 10010010b	; 1st flags, type flags
