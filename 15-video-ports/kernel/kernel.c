940668b36ac8bcb07a364455330fe43763b5053b
Vang3lis <852763852@qq.com>
2020-08-12 17:40:43 +0800

    I/O programming base

--- 15-video-ports/kernel/kernel.c
+++ 15-video-ports/kernel/kernel.c
@@ -15,4 +15,20 @@ void main() {
 	char *vga = 0xb8000;
 	vga[offset_from_vga] = 'X';
 	vga[offset_from_vga+1] = 0x0f;
+	
+	port_byte_out(0x3d4, 14);
+	port_byte_out(0x3d5, 0);
+	port_byte_out(0x3d4, 15);
+	port_byte_out(0x3d5, 0);
+	
+	int offset = 0;
+	offset = port_byte_in(0x3d5);
+	offset = offset << 8;
+	port_byte_out(0x3d4, 15);
+	offset += port_byte_in(0x3d5);
+	offset *= 2;
+
+	vga[offset] = 'Y';
+	vga[offset+1] = 0x0f;
+	
 }
